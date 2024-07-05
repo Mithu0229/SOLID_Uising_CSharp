ed2d2b7ed58c503ef84c3833e1e3775ab4b11324
Showkat Akbar mithu <showkat131080@gmail.com>
2024-07-06 01:03:45 +0600

    Single Responsibility Principle

---
+++ SOLID_Uising_CSharp/SRP/ReportGeneretor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Uising_CSharp.SRP
+{
+    public class ReportGenerators // this class are responsible for both method
+    {
+        public void GenerateReport()
+        {
+
+        }
+
+        public void FormatReport()
+        {
+
+        }
+    }
+
+    //The Single Responsibility Principle states that a class should have only one reason to change,
+    //meaning it should have only one job or responsibility.
+
+    public class ReportGenerator // one job
+    {
+        public void GenerateReport()
+        {
+
+        }
+    }
+
+    public class ReportFormatter // one job
+    {
+        public void FormatReport()
+        {
+
+        }
+    }
+}
