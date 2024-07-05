fc9771c7fe722accf0587dc0ea9b90775cc81451
Showkat Akbar mithu <showkat131080@gmail.com>
2024-07-06 01:12:39 +0600

    Open-Closed Principle

---
+++ SOLID_Uising_CSharp/OCP/Shape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Uising_CSharp.OCP
+{
+    //a class should be open for extension but closed for modification.
+    //This means that you should be able to add new functionality to a class without altering its existing code.
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+
+    //we can create specific classes for each shape, inheriting from an abstract base class Shape
+    public class Circle : Shape
+    {
+        public double Redius { get; set; }
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Redius, 2);
+        }
+    }
+
+    public class Square : Shape
+    {
+        public double SideLen { get; set; }
+        public override double CalculateArea()
+        {
+            return Math.Pow(SideLen, 2);
+        }
+    }
+}
