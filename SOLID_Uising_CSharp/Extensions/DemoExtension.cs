a62b9179295aab4beb90fdc43df1b60163605d36
Showkat Akbar mithu <showkat131080@gmail.com>
2024-07-07 21:41:28 +0600

    Extension method

---
+++ SOLID_Uising_CSharp/Extensions/DemoExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Uising_CSharp.Extensions
+{
+    public static class DemoExtension
+    {
+        public static string Revevers(this string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+    }
+
+    public class Main
+    {
+        public void TestExtension()
+        {
+            string s = "Hello word";
+            string vars = s.Revevers();
+            Console.WriteLine(vars);
+        }
+
+    }
+}
+
+
